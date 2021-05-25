8a517d383ed16120914a3b3b6d0b252e3786e736
Daniel Castilho <dan.castilho@gmail.com>
2021-05-25 00:20:01 -0400

    Apply Middleware Redux-Thunk

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -16,12 +16,9 @@ export const search = () => {
 };
 
 export const add = (description) => {
-	const req = axios.post(URL, { description });
-	return [
-		{
-			type: "TODO_ADDED",
-			payload: req,
-		},
-		search(),
-	];
+	return (dispatch) => {
+		axios.post(URL, { description })
+			.then((res) => dispatch({ type: "TODO_ADDED", payload: res.data }))
+			.then((res) => dispatch(search()));
+	};
 };
