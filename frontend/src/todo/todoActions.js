dec16a0dce1d57035ec17159de0b208863cd8894
Daniel Castilho <dan.castilho@gmail.com>
2021-05-24 23:45:39 -0400

    Action Creator Seach

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -1,4 +1,16 @@
+import axios from 'axios';
+
+const URL = "http://localhost:3000/api/todos"
+
 export const changeDescription = (event) => ({
 	type: "DESCRIPTION_CHANGED",
 	payload: event.target.value,
 });
+
+export const search = () => {
+	const req = axios.get(`${URL}?sort=-createdAt${search}`)
+	return {
+		type: 'TODO_SEARCHED',
+		payload: req
+	}
+}
